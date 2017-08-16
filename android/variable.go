1beb4005ec40b56e7bfb073c5e41d5bdd3d951c5
Luca Stefani <luca020400@lineageos.org>
2017-08-16 21:10:50 +0200

    config: Allow customizing LibartImgDeviceBaseAddress
    Change-Id: If5bb367d09035dfa4cd4cb03f600e8fc2978bf4d

--- android/variable.go
+++ android/variable.go
@@ -128,6 +128,7 @@ type productVariables struct {
 	Eng                        *bool `json:",omitempty"`
 	EnableCFI                  *bool `json:",omitempty"`
 	Treble                     *bool `json:",omitempty"`
+	Libart_img_base            *string `json:",omitempty"`
 
 	VendorPath *string `json:",omitempty"`
 
