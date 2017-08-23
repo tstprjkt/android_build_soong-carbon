e20211741a16303dfe5278274297ebb8d95684e0
Simon Shields <simon@lineageos.org>
2017-08-23 12:05:33 +1000

    soong: extend with carbon board flags
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3

--- scripts/microfactory.bash
+++ scripts/microfactory.bash
@@ -59,7 +59,9 @@ function soong_build_go
     BUILDDIR=$(getoutdir) \
       SRCDIR=${TOP} \
       BLUEPRINTDIR=${TOP}/build/blueprint \
-      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong -pkg-path github.com/golang/protobuf=${TOP}/external/golang-protobuf" \
+      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong
+                  -pkg-path github.com/golang/protobuf=${TOP}/external/golang-protobuf
+                  -pkg-path carbon/soong=${TOP}/vendor/carbon/build/soong" \
       build_go $@
 }
 
