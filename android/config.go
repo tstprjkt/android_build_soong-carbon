e2dddceebf2d46e75680ba10d4a615d5032c9d99
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-31 20:00:40 -0700

    soong: Add function to return camera parameters library name
    Change-Id: I3113ea6813b82c3f6b250205080030ff9c2255ec

--- android/config.go
+++ android/config.go
@@ -527,6 +527,10 @@ func (c *deviceConfig) DeviceKernelHeaderDirs() []string {
 	return c.config.ProductVariables.DeviceKernelHeaders
 }
 
+func (c *deviceConfig) SpecificCameraParametersLibrary() string {
+	return String(c.config.ProductVariables.Carbon.Specific_camera_parameter_library)
+}
+
 func (c *deviceConfig) NativeCoverageEnabled() bool {
 	return Bool(c.config.ProductVariables.NativeCoverage)
 }
