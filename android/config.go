CarbonROM/android_build_soong

soong: handle qcom audio/display/media paths
Change-Id: Iead25f930b8bca26d31450f9709a44a8c23b3031

android/config.go
@@ -505,3 +505,15 @@ func (c *deviceConfig) CoverageEnabledForPath(path string) bool {
 func (c *deviceConfig) BoardUsesQTIHardware() bool {
 	return Bool(c.config.ProductVariables.BoardUsesQTIHardware)
 }
+
+func (c *deviceConfig) QTIAudioPath() string {
+	return String(c.config.ProductVariables.QTIAudioPath)
+}
+
+func (c *deviceConfig) QTIDisplayPath() string {
+	return String(c.config.ProductVariables.QTIDisplayPath)
+}
+
+func (c *deviceConfig) QTIMediaPath() string {
+	return String(c.config.ProductVariables.QTIMediaPath)
+}

tstprjkt/android_build_soong-carbon

android/config.go
@@ -1057,3 +1057,15 @@ func (c *config) ProductHiddenAPIStubsTest() []string {
 func (c *deviceConfig) TargetFSConfigGen() []string {
 	return c.config.productVariables.TargetFSConfigGen
 }
+
+func (c *deviceConfig) QTIAudioPath() string {
+	return String(c.config.productVariables.QTIAudioPath)
+}
+
+func (c *deviceConfig) QTIDisplayPath() string {
+	return String(c.config.productVariables.QTIDisplayPath)
+}
+
+func (c *deviceConfig) QTIMediaPath() string {
+	return String(c.config.productVariables.QTIMediaPath)
+}